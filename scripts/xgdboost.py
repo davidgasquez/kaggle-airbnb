e20897c04a5d8e191633c3c040e445c121d6d6bc
David Gasquez <davidgasquez@gmail.com>
2015-12-15 00:17:25 +0100

    Apply PEP8 format

--- scripts/xgdboost.py
+++ scripts/xgdboost.py
@@ -3,75 +3,95 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from xgboost.sklearn import XGBClassifier
 
-np.random.seed(0)
+# Data loading
+train_users = pd.read_csv('data/raw/train_users.csv')
+test_users = pd.read_csv('data/raw/test_users.csv')
+labels = train_users['country_destination'].values
+train_users = train_users.drop(['country_destination'], axis=1)
+id_test = test_users['id']
+piv_train = train_users.shape[0]
 
-#Loading data
-df_train = pd.read_csv('data/raw/train_users.csv')
-df_test = pd.read_csv('data/raw/test_users.csv')
-labels = df_train['country_destination'].values
-df_train = df_train.drop(['country_destination'], axis=1)
-id_test = df_test['id']
-piv_train = df_train.shape[0]
+# Join train and test users
+users = pd.concat((train_users, test_users), axis=0, ignore_index=True)
 
-#Creating a DataFrame with train+test data
-df_all = pd.concat((df_train, df_test), axis=0, ignore_index=True)
-#Removing id and date_first_booking
-df_all = df_all.drop(['id', 'date_first_booking'], axis=1)
-#Filling nan values
-df_all.loc[df_all.age > 500, 'age'] = 2015 - df_all.age
-df_all = df_all.fillna(-1)
+# Drop ID and date_first_booking from the DataFrame
+users = users.drop(['id', 'date_first_booking'], axis=1)
 
+# Fill NaN values
+users = users.fillna(-1)  # TODO: Try with NaN
 
-#####Feature engineering#######
-#date_account_created
-dac = np.vstack(df_all.date_account_created.astype(str).apply(lambda x: list(map(int, x.split('-')))).values)
-df_all['dac_year'] = dac[:,0]
-df_all['dac_month'] = dac[:,1]
-df_all['dac_day'] = dac[:,2]
-df_all = df_all.drop(['date_account_created'], axis=1)
+###############################################################################
+#                                Preprocessing                                #
+###############################################################################
 
-#timestamp_first_active
-tfa = np.vstack(df_all.timestamp_first_active.astype(str).apply(lambda x: list(map(int, [x[:4],x[4:6],x[6:8],x[8:10],x[10:12],x[12:14]]))).values)
-df_all['tfa_year'] = tfa[:,0]
-df_all['tfa_month'] = tfa[:,1]
-df_all['tfa_day'] = tfa[:,2]
-df_all = df_all.drop(['timestamp_first_active'], axis=1)
+# Date Account Creation
+dac = np.vstack(users.date_account_created.astype(str).apply(lambda x: list(map(int, x.split('-')))).values)
+users['dac_year'] = dac[:,0]
+users['dac_month'] = dac[:,1]
+users['dac_day'] = dac[:,2]
+users = users.drop(['date_account_created'], axis=1)
 
-#Age
-av = df_all.age.values
-df_all['age'] = np.where(np.logical_or(av<14, av>100), -1, av)
+# Timestamp First Active
+tfa = np.vstack(users.timestamp_first_active.astype(str).apply(lambda x: list(map(int, [x[:4],x[4:6],x[6:8],x[8:10],x[10:12],x[12:14]]))).values)
+users['tfa_year'] = tfa[:,0]
+users['tfa_month'] = tfa[:,1]
+users['tfa_day'] = tfa[:,2]
+users = users.drop(['timestamp_first_active'], axis=1)
 
-#One-hot-encoding features
-ohe_feats = ['gender', 'signup_method', 'signup_flow', 'language',
-'affiliate_channel', 'affiliate_provider', 'first_affiliate_tracked',
-'signup_app', 'first_device_type', 'first_browser']
-for f in ohe_feats:
-    df_all_dummy = pd.get_dummies(df_all[f], prefix=f)
-    df_all = df_all.drop([f], axis=1)
-    df_all = pd.concat((df_all, df_all_dummy), axis=1)
+# Age
+av = users.age.values
+users['age'] = np.where(np.logical_or(av < 14, av > 100), -1, av)
 
-#Splitting train and test
-vals = df_all.values
-X = vals[:piv_train]
+# One-hot-Encoding categorical features
+categorical_features = [
+    'gender', 'signup_method', 'signup_flow', 'language', 'affiliate_channel',
+    'affiliate_provider', 'first_affiliate_tracked', 'signup_app',
+    'first_device_type', 'first_browser'
+]
+
+for category in categorical_features:
+    users_dummy = pd.get_dummies(users[category], prefix=category)
+    users = users.drop([category], axis=1)
+    users = pd.concat((users, users_dummy), axis=1)
+
+# Splitting train and test
+values = users.values
+X = values[:piv_train]
 le = LabelEncoder()
 y = le.fit_transform(labels)
-X_test = vals[piv_train:]
+X_test = values[piv_train:]
 
-#Classifier
-xgb = XGBClassifier(max_depth=6, learning_rate=0.25, n_estimators=43,
-                    objective='multi:softprob', subsample=0.6,
-                    colsample_bytree=0.6, seed=0)
+# Classifier
+xgb = XGBClassifier(
+    max_depth=10,
+    learning_rate=0.22,
+    n_estimators=100,
+    objective="multi:softprob",
+    nthread=-1,
+    gamma=0,
+    min_child_weight=1,
+    max_delta_step=0,
+    subsample=0.6,
+    colsample_bytree=0.6,
+    colsample_bylevel=1,
+    reg_alpha=0.1,
+    reg_lambda=0.9,
+    scale_pos_weight=1,
+    base_score=0.5,
+    seed=42
+)
+
 xgb.fit(X, y)
 y_pred = xgb.predict_proba(X_test)
 
-#Taking the 5 classes with highest probabilities
-ids = []  #list of ids
-cts = []  #list of countries
+# Taking the 5 classes with highest probabilities
+ids = []
+cts = []
 for i in range(len(id_test)):
     idx = id_test[i]
     ids += [idx] * 5
     cts += le.inverse_transform(np.argsort(y_pred[i])[::-1])[:5].tolist()
 
-#Generate submission
+# Generate Submission
 sub = pd.DataFrame(np.column_stack((ids, cts)), columns=['id', 'country'])
-sub.to_csv('data/submissions/sub.csv',index=False)
+sub.to_csv('data/submissions/xgboost.csv',index=False)
