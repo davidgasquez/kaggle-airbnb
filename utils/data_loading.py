237ca384f639b8c88908b5bf00a370fd8b3b48a4
David Gasquez <davidgasquez@gmail.com>
2015-12-16 11:03:22 +0100

    Add comments :memo:

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -4,9 +4,12 @@ DEFAULT_PATH = '../data/raw/'
 
 
 def load_users_data(path=DEFAULT_PATH):
+    """Loads users data into train and test users."""
     train_users = pd.read_csv(path + 'train_users.csv')
     test_users = pd.read_csv(path + 'test_users.csv')
     return train_users, test_users
 
+
 def load_sessions_data(path=DEFAULT_PATH):
+    """Loads the users sessions data."""
     return pd.read_csv(path + 'sessions.csv')
