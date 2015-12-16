93b8a2a0e9dd92c3f2117d20bb251fb888da1379
David Gasquez <davidgasquez@gmail.com>
2015-12-16 12:23:34 +0100

    More comments vervosity

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -1,15 +1,40 @@
+"""Wrappers to simplify data loading."""
+
 import pandas as pd
 
+# Set default path
 DEFAULT_PATH = '../data/raw/'
 
 
 def load_users_data(path=DEFAULT_PATH):
-    """Loads users data into train and test users."""
+    """Loads users data into train and test users.
+
+    Parameters
+    ----------
+    path: str
+        Path of the folder containing the data.
+
+    Returns
+    -------
+    train_users, test_users: DataFrame, DataFrame
+        Loaded DataFrames.
+    """
     train_users = pd.read_csv(path + 'train_users.csv')
     test_users = pd.read_csv(path + 'test_users.csv')
     return train_users, test_users
 
 
 def load_sessions_data(path=DEFAULT_PATH):
-    """Loads the users sessions data."""
+    """Loads the users sessions data.
+
+    Parameters
+    ----------
+    path: str
+        Path of the folder containing the data.
+
+    Returns
+    -------
+    sessions: DataFrame
+        Loaded DataFrame.
+    """
     return pd.read_csv(path + 'sessions.csv')
