b5ef4fd649edc1dfe7e439ac771f3cbc40ae5cc3
David Gasquez <davidgasquez@gmail.com>
2015-12-18 15:18:27 +0100

    Add preprocessing draft

---
+++ utils/preprocessing.py
@@ -0,0 +1,11 @@
+from sklearn.svm import SVC
+
+
+def SVMI(data, column, missing_values='NaN'):
+    # train_data = DataFrame with data[column] different than missing values
+    # test_data = DataFrame with data[column] equal than missing values
+    # set X, and y from train_data
+    # drop column from test_data
+    # fit and predict
+
+    pass
