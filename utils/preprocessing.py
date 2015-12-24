90863680bc918288d2330900d5216fafb44581b8
David Gasquez <davidgasquez@gmail.com>
2015-12-24 19:44:17 +0100

    Clean file

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,8 +1,6 @@
 """Utils used in the preprocessing stage"""
 
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 
 
 def one_hot_encoding(data, categorical_features):
@@ -10,52 +8,19 @@ def one_hot_encoding(data, categorical_features):
 
     Parameters
     ----------
-    data :
-    categorical_features :
+    data : Pandas DataFrame
+        DataFrame containing the categorical features.
+    categorical_features : list
+        Names of the categorial features to encode
 
     Returns
     -------
     data : DataFrame
-        Returns a pandas DataFrame with new binary columns.
+        Returns a pandas DataFrame with one-hot scheme binary columns.
     """
     for feature in categorical_features:
         data_dummy = pd.get_dummies(data[feature], prefix=feature)
-        data = data.drop([feature], axis=1)
+        data.drop([feature], axis=1, inplace=True)
         data = pd.concat((data, data_dummy), axis=1)
-
-    return data
-
-
-def input_missing_values(data, column, model, scale=True):
-    """Input missing values into the DataFrame using SVM.
-
-    Parameters
-    ----------
-    data :
-    column :
-
-    Returns
-    -------
-    data : DataFrame
-        Returns a pandas DataFrame with the feature column filled with
-        predictions.
-    """
-    df = data
-    null_values = df[column].isnull()
-
-    if scale:
-        X = df.columns.tolist()
-        X.remove(column)
-
-        df[X] = StandardScaler().fit_transform(df[X])
-
-    model.fit(
-        df[~null_values].drop(column, axis=1),
-        df[~null_values][column]
-    )
-
-    predicted_values = model.predict(df[null_values].drop(column, axis=1))
-
-    data.loc[null_values, column] = np.around(predicted_values)
 
     return data
