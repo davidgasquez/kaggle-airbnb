9a20ce0ed6e84f23c2af65992f7940284b6b3340
David Gasquez <davidgasquez@gmail.com>
2015-12-26 12:31:54 +0100

    Add CV parameters

--- scripts/cv.py
+++ scripts/cv.py
@@ -1,72 +1,84 @@
 import sys
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 sys.path.append('..')
-from utils.preprocessing import one_hot_encoding
 from utils.data_loading import load_users_data
-from sklearn.preprocessing import LabelEncoder
+from utils.preprocessing import one_hot_encoding
 
+print "Loading data...",
+train_users, test_users = load_users_data
+users = pd.read_csv('../datasets/processed/users_with_session.csv')
+print "\tDONE"
 
-print 'START'
+print "Preprocessing...",
 
-print 'Loading data...',
-
-train_users, test_users = load_users_data()
+# Get train labels and ids
 labels = train_users['country_destination'].values
 train_users = train_users.drop(['country_destination'], axis=1)
-
-print '\tDONE'
-
-print 'Preprocessing...',
-
 id_test = test_users['id']
 piv_train = train_users.shape[0]
-users = pd.concat((train_users, test_users), axis=0, ignore_index=True)
-users = users.drop(['id', 'date_first_booking'], axis=1)
 
-# Fill NaN values
+drop_list = [
+    'id',
+    'country_destination',
+    'Unnamed: 0',
+    'date_account_created',
+    'date_first_active',
+    'timestamp_first_active'
+]
+
+# Drop columns
+users = users.drop(drop_list, axis=1)
+
+# Fill NaNs
 users = users.fillna(-1)
 
-users['date_account_created'] = pd.to_datetime(users['date_account_created'])
-users['year_account_created'] = pd.DatetimeIndex(users['date_account_created']).year
-users['month_account_created'] = pd.DatetimeIndex(users['date_account_created']).month
-users['day_account_created'] = pd.DatetimeIndex(users['date_account_created']).day
-users = users.drop(['date_account_created'], axis=1)
-users['timestamp_first_active'] = pd.to_datetime(users['timestamp_first_active'], format='%Y%m%d%H%M%S')
-users['year_first_active'] = pd.DatetimeIndex(users['timestamp_first_active']).year
-users['month_first_active'] = pd.DatetimeIndex(users['timestamp_first_active']).month
-users['day_first_active'] = pd.DatetimeIndex(users['timestamp_first_active']).day
-users = users.drop(['timestamp_first_active'], axis=1)
-age_values = users.age.values
-users['age'] = np.where(np.logical_or(age_values < 14, age_values > 100), -1, age_values)
-
+# Encode categorical features
 categorical_features = [
     'gender', 'signup_method', 'signup_flow', 'language', 'affiliate_channel',
     'affiliate_provider', 'first_affiliate_tracked', 'signup_app',
-    'first_device_type', 'first_browser'
+    'first_device_type', 'first_browser', 'most_used_device'
 ]
-
 users = one_hot_encoding(users, categorical_features)
 
 # Splitting train and test
 values = users.values
+values = StandardScaler().fit_transform(values)
 X = values[:piv_train]
 le = LabelEncoder()
 y = le.fit_transform(labels)
 X_test = values[piv_train:]
-print '\tDONE'
+
+print "\tDONE"
 
 import xgboost
 from sklearn.grid_search import GridSearchCV
 
-xgb_model = xgboost.XGBClassifier(nthread=-1)
+xgb_model = xgboost.XGBClassifier(
+    objective="multi:softprob",
+    nthread=-1,
+    gamma=0,
+    min_child_weight=1,
+    max_delta_step=0,
+    subsample=0.6,
+    colsample_bytree=0.6,
+    colsample_bylevel=1,
+    reg_alpha=0,
+    reg_lambda=1,
+    scale_pos_weight=1,
+    base_score=0.5,
+    seed=42
+)
 
 clf = GridSearchCV(
     xgb_model,
     {
         'max_depth':[1,2,3,4,6],
-        'n_estimators': [20,25,30,35,40]
+        'n_estimators': [20,25,30,35,40],
+        'learning_rate':[0.2, 0.4, 0.1],
     },
     cv=2,
     verbose=2,
@@ -76,6 +88,8 @@ clf = GridSearchCV(
 
 clf.fit(X,y)
 
+print
 print(clf.grid_scores_)
+print
 print(clf.best_score_)
 print(clf.best_params_)
