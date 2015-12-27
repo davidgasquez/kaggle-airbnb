bda6cbfc28a09eec61bb3a94f7f9a7667b1f0a3e
David Gasquez <davidgasquez@gmail.com>
2015-12-27 19:59:19 +0100

    Sync Gradient Boosting

--- scripts/gradient_boosting.py
+++ scripts/gradient_boosting.py
@@ -22,8 +22,8 @@ def generate_submission(y_pred, test_users_ids, label_encoder):
 
 def main():
     path = '../datasets/processed/'
-    train_users = pd.read_csv(path + 'processed_train_users.csv')
-    test_users = pd.read_csv(path + 'processed_test_users.csv')
+    train_users = pd.read_csv(path + 'train_users.csv')
+    test_users = pd.read_csv(path + 'test_users.csv')
 
     y_train = train_users['country_destination']
     train_users.drop('country_destination', axis=1, inplace=True)
