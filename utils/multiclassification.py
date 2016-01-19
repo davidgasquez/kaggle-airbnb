c8552ac8d96d75210b6581ff3a53e1e2c8984f95
David Gasquez <davidgasquez@gmail.com>
2016-01-19 17:26:35 +0100

    :art:

--- utils/multiclassification.py
+++ utils/multiclassification.py
@@ -1,3 +1,8 @@
+"""Multiclass and multilabel classification strategies.
+
+This module implements an one-vs-one multiclass learning algorithm that uses
+SMOTE algorithm to over sample the minority class in each fit.
+"""
 from __future__ import division
 
 import numpy as np
@@ -21,7 +26,6 @@ def _fit_ovo_binary(estimator, X, y, i, j, sampling=None, verbose=False):
     X_values = X[ind[cond]]
     y_values = y_binary
 
-    # TODO: Save X_values, and y_values to speed up calculations
     if sampling:
         ones = np.count_nonzero(y_values == 1)
         zeros = np.count_nonzero(y_values == 0)
@@ -40,24 +44,71 @@ def _fit_ovo_binary(estimator, X, y, i, j, sampling=None, verbose=False):
 
 
 class CustomOneVsOneClassifier(OneVsOneClassifier):
+    """One-vs-one multiclass strategy.
+
+    This strategy consists in fitting one classifier per class pair.
+    At prediction time, the class which received the most votes is selected.
+
+    Requires to fit `n_classes * (n_classes - 1) / 2` classifiers.
+
+    Attributes
+    ----------
+    estimators_ : list of `n_classes * (n_classes - 1) / 2` estimators
+        Estimators used for predictions.
+    classes_ : numpy array of shape [n_classes]
+        Array containing labels.
+    """
 
     def __init__(self, estimator, n_jobs=1, sampling=None, verbose=False):
+        """Init method.
+
+        Parameters
+        ----------
+        estimator : estimator object
+            An estimator object implementing fit and one of decision_function
+            or predict_proba.
+        n_jobs : int, optional, default: 1
+            The number of jobs to use. If -1 all CPUs are used.
+            If 1 is given, no parallel computing code is used at all, which is
+            useful for debugging. For n_jobs below -1, (n_cpus + 1 + n_jobs)
+            are used. Thus for n_jobs = -2, all CPUs but one are used.
+        sampling : str, optional default:None
+            Samplig method to use when fitting each estimator.
+            Can be 'SMOTE' or SMOTEENN'.
+        """
         self.estimator = estimator
         self.n_jobs = n_jobs
         self.sampling = sampling
         self.verbose = verbose
 
     def predict_proba(self, X):
+        """Predict class probabilities for X.
+
+        The predicted class probabilities of an input sample is the same as
+        the result of decision_function.
+
+        Parameters
+        ----------
+        X : array-like or sparse matrix of shape = [n_samples, n_features]
+
+        Returns
+        -------
+        p : array of shape = [n_samples, n_classes], or a list of n_outputs
+            such arrays if n_outputs > 1.
+            The class probabilities of the input samples.
+        """
         return super(CustomOneVsOneClassifier, self).decision_function(X)
 
     def fit(self, X, y):
         """Fit underlying estimators.
+
         Parameters
         ----------
         X : (sparse) array-like, shape = [n_samples, n_features]
             Data.
         y : array-like, shape = [n_samples]
             Multi-class targets.
+
         Returns
         -------
         self
@@ -66,15 +117,11 @@ class CustomOneVsOneClassifier(OneVsOneClassifier):
 
         self.classes_ = np.unique(y)
         n_classes = self.classes_.shape[0]
+
         self.estimators_ = Parallel(n_jobs=self.n_jobs)(
             delayed(_fit_ovo_binary)(
-                self.estimator,
-                X,
-                y,
-                self.classes_[i],
-                self.classes_[j],
-                sampling=self.sampling,
-                verbose=self.verbose
+                self.estimator, X, y, self.classes_[i], self.classes_[j],
+                sampling=self.sampling, verbose=self.verbose
             ) for i in range(n_classes) for j in range(i + 1, n_classes))
 
         return self
