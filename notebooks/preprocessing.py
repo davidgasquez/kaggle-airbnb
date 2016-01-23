9dbecdd6f1a51706268c5f7421bc6f9061678990
David Gasquez <davidgasquez@gmail.com>
2016-01-23 13:39:11 +0100

    Update preprocessing script

--- notebooks/preprocessing.py
+++ notebooks/preprocessing.py
@@ -7,7 +7,7 @@ from utils.preprocessing import one_hot_encoding
 from utils.preprocessing import get_weekday
 
 
-def process_user_session(user):
+def process_user_actions(user):
     user_session = sessions.loc[sessions['user_id'] == user]
     # Get the user session
     user_session_data = pd.Series()
@@ -16,17 +16,15 @@ def process_user_session(user):
     user_session_data['session_lenght'] = len(user_session)
     user_session_data['id'] = user
 
-    suffix = '_secs_elapsed'
-
     for column in ['action', 'action_type', 'action_detail', 'device_type']:
-        column_data = user_session.groupby(column).secs_elapsed.sum()
-        column_data.rename(lambda x: x + suffix, inplace=True)
+        column_data = user_session[column].value_counts()
+        column_data.index = column_data.index + '_count'
         user_session_data = user_session_data.append(column_data)
 
     # Get the most used device
     user_session_data['most_used_device'] = user_session['device_type'].max()
 
-    return user_session_data.groupby(level=0).sum()
+    return user_session_data.groupby(user_session_data.index).sum()
 
 
 def process_user_secs_elapsed(user):
@@ -50,15 +48,14 @@ def process_user_secs_elapsed(user):
 raw_data_path = '../data/raw/'
 processed_data_path = '../data/processed/'
 
-global sessions
-
 # Load raw data
 train_users = pd.read_csv(raw_data_path + 'train_users.csv')
 test_users = pd.read_csv(raw_data_path + 'test_users.csv')
-sessions = pd.read_csv(raw_data_path + 'sessions.csv')
+sessions = pd.read_csv(raw_data_path + 'sessions.csv', nrows=2000000)
 
 # Join users
 users = pd.concat((train_users, test_users), axis=0, ignore_index=True)
+users = users.set_index('id')
 
 # Drop date_first_booking column (empty since competition's restart)
 users = users.drop('date_first_booking', axis=1)
@@ -96,26 +93,25 @@ day_first_active = pd.DatetimeIndex(users['date_first_active']).day
 users['day_first_active'] = day_first_active
 
 p = Pool(multiprocessing.cpu_count())
-processed_sessions = p.map(process_user_session, sessions['user_id'].unique())
-user_sessions = pd.DataFrame(processed_sessions).set_index('id')
+result = p.map(process_user_actions, sessions['user_id'].unique())
+result = pd.DataFrame(result).set_index('id')
+users = pd.concat([users, result], axis=1)
 
-# Joint the processed data with each user
-users = users.set_index('id')
-users = pd.concat([users, user_sessions], axis=1)
+print 'user_actions'
 
 # TODO: Classify by dispositive
 
 # Get the count of general session information
-user_sessions = sessions.groupby('user_id').count()
-user_sessions.rename(columns=lambda x: x + '_count', inplace=True)
-users = pd.concat([users, user_sessions], axis=1)
+result = sessions.groupby('user_id').count()
+result.rename(columns=lambda x: x + '_count', inplace=True)
+users = pd.concat([users, result], axis=1)
 
 p = Pool(multiprocessing.cpu_count())
-processed_secs_elapsed = p.map(
-    process_user_secs_elapsed, sessions['user_id'].unique())
-processed_secs_elapsed = pd.DataFrame(processed_secs_elapsed).set_index('id')
+result = p.map(process_user_secs_elapsed, sessions['user_id'].unique())
+result = pd.DataFrame(result).set_index('id')
+users = pd.concat([users, result], axis=1)
 
-users = pd.concat([users, processed_secs_elapsed], axis=1)
+print 'seconds_info'
 
 train_users = train_users.set_index('id')
 test_users = test_users.set_index('id')
@@ -125,35 +121,50 @@ processed_train_users = users.loc[train_users.index]
 processed_test_users = users.loc[test_users.index]
 processed_test_users.drop(['country_destination'], inplace=True, axis=1)
 
-processed_train_users.to_csv(processed_data_path + 'processed_train_users.csv')
-processed_test_users.to_csv(processed_data_path + 'processed_test_users.csv')
+suffix = 'count_processed_'
+processed_train_users.to_csv(processed_data_path + suffix + 'train_users.csv')
+processed_test_users.to_csv(processed_data_path + suffix + 'test_users.csv')
 
-drop_list = [
-    'date_account_created',
-    'date_first_active',
-    'timestamp_first_active'
-]
-
-# Drop columns
-users = users.drop(drop_list, axis=1)
-
-# TODO: Try with StandardScaler
-# from sklearn.preprocessing import StandardScaler
-# scaler = StandardScaler()
-# scaler.fit_transform(users)
-
-# Encode categorical features
-categorical_features = [
-    'gender', 'signup_method', 'signup_flow', 'language', 'affiliate_channel',
-    'affiliate_provider', 'first_affiliate_tracked', 'signup_app',
-    'first_device_type', 'first_browser', 'most_used_device'
-]
-
-users = one_hot_encoding(users, categorical_features)
-
-processed_train_users = users.loc[train_users.index]
-processed_test_users = users.loc[test_users.index]
-processed_test_users.drop('country_destination', inplace=True, axis=1)
-
-processed_train_users.to_csv(processed_data_path + 'encoded_train_users.csv')
-processed_test_users.to_csv(processed_data_path + 'encoded_test_users.csv')
+# drop_list = [
+#     'date_account_created',
+#     'date_first_active',
+#     'timestamp_first_active'
+# ]
+#
+# # Drop columns
+# users.drop(drop_list, axis=1, inplace=True)
+#
+# # TODO: Try with StandardScaler
+# # from sklearn.preprocessing import StandardScaler
+# # scaler = StandardScaler()
+# # scaler.fit_transform(users)
+#
+# tr_i = train_users.index
+# te_i = test_users.index
+#
+# train_users = 0
+# test_users = 0
+# sessions = 0
+# result = 0
+# processed_train_users = 0
+# processed_test_users = 0
+#
+# import gc
+# gc.collect()
+#
+# # Encode categorical features
+# categorical_features = [
+#     'gender', 'signup_method', 'signup_flow', 'language', 'affiliate_channel',
+#     'affiliate_provider', 'first_affiliate_tracked', 'signup_app',
+#     'first_device_type', 'first_browser', 'most_used_device'
+# ]
+#
+# users = one_hot_encoding(users, categorical_features)
+#
+# processed_train_users = users.loc[tr_i]
+# processed_test_users = users.loc[te_i]
+# processed_test_users.drop('country_destination', inplace=True, axis=1)
+#
+# suffix = 'ohe_count_processed_'
+# processed_train_users.to_csv(processed_data_path + suffix + 'train_users.csv')
+# processed_test_users.to_csv(processed_data_path + suffix + 'test_users.csv')
