d95b6cd121f566072776efcbb4957b1db3733f96
David Gasquez <davidgasquez@gmail.com>
2016-01-27 19:38:36 +0100

    Add IDEA

--- scripts/feature_engeenering.py
+++ scripts/feature_engeenering.py
@@ -29,6 +29,7 @@ def process_user_actions(user):
 
     # Take the count of each value per column
     for column in ['action', 'action_type', 'action_detail', 'device_type']:
+        # IDEA: Limit with head()
         column_data = user_session[column].value_counts()
         column_data.index = column_data.index + '_count'
         user_session_data = user_session_data.append(column_data)
