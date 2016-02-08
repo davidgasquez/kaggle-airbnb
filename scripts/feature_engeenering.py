9a431f081d0d56755df9ee17283b69629b7a5660
David Gasquez <davidgasquez@gmail.com>
2016-02-08 22:37:43 +0100

    Change nrows

--- scripts/feature_engeenering.py
+++ scripts/feature_engeenering.py
@@ -8,7 +8,7 @@ from kairbnb.preprocessing import process_user_actions
 from kairbnb.preprocessing import process_user_secs_elapsed
 from kairbnb.io import load_users
 
-NROWS = 100000
+NROWS = None
 VERSION = '5'
 
 if __name__ == '__main__':
