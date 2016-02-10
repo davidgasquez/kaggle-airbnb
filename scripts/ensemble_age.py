38295cb82c64234925f408053579cb07bb70643c
David Gasquez <davidgasquez@gmail.com>
2016-02-10 15:51:49 +0100

    Add new ensemble method

--- scripts/ensemble_age.py
+++ scripts/ensemble_age.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import LabelEncoder
 from xgboost.sklearn import XGBClassifier
 import pandas as pd
 
-from kairbnb.io import generate_submission, load_users
+from kairbnb.io import generate_submission
 
 train_users = pd.read_csv('../cache/train_users.csv1')
 test_users = pd.read_csv('../cache/test_users.csv1')
@@ -30,7 +30,7 @@ clf = XGBClassifier(
     min_child_weight=1,
     max_delta_step=0,
     subsample=1,
-    colsample_bytree=0.8,
+    colsample_bytree=1,
     colsample_bylevel=1,
     reg_alpha=0,
     reg_lambda=1,
@@ -84,8 +84,6 @@ clf = XGBClassifier(
 clf.fit(x_train, encoded_y_train)
 y_2_pred = clf.predict_proba(x_test)
 
-
-
 train_users = pd.read_csv('../cache/train_users_with_age_et.csv')
 test_users = pd.read_csv('../cache/test_users_with_age_et.csv')
 
@@ -126,6 +124,6 @@ clf.fit(x_train, encoded_y_train)
 y_3_pred = clf.predict_proba(x_test)
 
 
-y_pred = (y_pred * 0.6) + (y_2_pred * 0.2) + (y_3_pred * 0.2)
+y_pred = (y_pred * 0.8) + (y_2_pred * 0.1) + (y_3_pred * 0.1)
 
-generate_submission(y_pred, test_users_ids, label_encoder, name='ensemble')
+generate_submission(y_pred, test_users_ids, label_encoder, name='ensemble_age')
---
+++ scripts/ensemble_date.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+from sklearn.preprocessing import LabelEncoder
+from xgboost.sklearn import XGBClassifier
+import pandas as pd
+
+from kairbnb.io import generate_submission
+
+train_users = pd.read_csv('../cache/train_users.csv1')
+test_users = pd.read_csv('../cache/test_users.csv1')
+
+x_train_users = train_users
+print(x_train_users.shape)
+
+y_train = x_train_users['country_destination']
+x_train_users = x_train_users.drop(['country_destination', 'id'], axis=1)
+x_train_users = x_train_users.fillna(-1)
+
+x_train = x_train_users.values
+label_encoder = LabelEncoder()
+encoded_y_train = label_encoder.fit_transform(y_train)
+
+test_users_ids = test_users['id']
+test_users.drop('id', axis=1, inplace=True)
+test_users = test_users.fillna(-1)
+x_test = test_users.values
+
+clf = XGBClassifier(
+    max_depth=7,
+    learning_rate=0.15,
+    n_estimators=80,
+    objective="rank:pairwise",
+    gamma=0,
+    min_child_weight=1,
+    max_delta_step=0,
+    subsample=1,
+    colsample_bytree=1,
+    colsample_bylevel=1,
+    reg_alpha=0,
+    reg_lambda=1,
+    scale_pos_weight=1,
+    base_score=0.5,
+    missing=None,
+    silent=True,
+    nthread=-1,
+    seed=42
+)
+
+clf.fit(x_train, encoded_y_train)
+y_pred = clf.predict_proba(x_test)
+
+months = train_users['month_account_created'] > 0
+years = train_users['year_account_created'] > 2013
+
+x_train_users = train_users.loc[months & years]
+print(x_train_users.shape)
+
+y_train = x_train_users['country_destination']
+x_train_users = x_train_users.drop(['country_destination', 'id'], axis=1)
+x_train_users = x_train_users.fillna(-1)
+
+x_train = x_train_users.values
+label_encoder = LabelEncoder()
+encoded_y_train = label_encoder.fit_transform(y_train)
+
+clf = XGBClassifier(
+    max_depth=7,
+    learning_rate=0.15,
+    n_estimators=80,
+    objective="rank:pairwise",
+    gamma=0,
+    min_child_weight=1,
+    max_delta_step=0,
+    subsample=1,
+    colsample_bytree=0.8,
+    colsample_bylevel=1,
+    reg_alpha=0,
+    reg_lambda=1,
+    scale_pos_weight=1,
+    base_score=0.5,
+    missing=None,
+    silent=True,
+    nthread=-1,
+    seed=42
+)
+
+clf.fit(x_train, encoded_y_train)
+y_pred_2 = clf.predict_proba(x_test)
+
+months = train_users['month_account_created'] > 2
+years = train_users['year_account_created'] > 2014
+
+x_train_users = train_users.loc[months & years]
+print(x_train_users.shape)
+
+y_train = x_train_users['country_destination']
+x_train_users = x_train_users.drop(['country_destination', 'id'], axis=1)
+x_train_users = x_train_users.fillna(-1)
+
+x_train = x_train_users.values
+label_encoder = LabelEncoder()
+encoded_y_train = label_encoder.fit_transform(y_train)
+
+clf = XGBClassifier(
+    max_depth=7,
+    learning_rate=0.15,
+    n_estimators=80,
+    objective="rank:pairwise",
+    gamma=0,
+    min_child_weight=1,
+    max_delta_step=0,
+    subsample=1,
+    colsample_bytree=0.8,
+    colsample_bylevel=1,
+    reg_alpha=0,
+    reg_lambda=1,
+    scale_pos_weight=1,
+    base_score=0.5,
+    missing=None,
+    silent=True,
+    nthread=-1,
+    seed=42
+)
+
+clf.fit(x_train, encoded_y_train)
+y_pred_3 = clf.predict_proba(x_test)
+
+generate_submission(y_pred, test_users_ids, label_encoder, name='ensemble_date')
